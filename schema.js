f5851e88a150643da721963c78c2aa0fbd771b53
Ben Du <82544666+ben-du1@users.noreply.github.com>
2024-09-27 07:56:55 -0700

    e

--- schema.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('thinkingclub')
-
-db.serialize(() => {
-    db.run("CREATE TABLE meetings (date TEXT, title TEXT, location TEXT, info TEXT)")
-})
-
-db.close()
